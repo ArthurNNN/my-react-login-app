5178f174f8d2750a035b72942dab156b7be15428
ArthurNNN <nikitinarthur@gmail.com>
2021-06-12 14:40:05 +0200

    Login, Register and Home pages created

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+
+function Navigation(props) {
+  return (
+    <div className="navigation">
+      <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <div class="container">
+          <Link class="navbar-brand" to="/">
+            React Multi-Page Website
+          </Link>
+          <div>
+            <ul class="navbar-nav ml-auto">
+              <li
+                class={`nav-item  ${
+                  props.location.pathname === "/" ? "active" : ""
+                }`}
+              >
+                <Link class="nav-link" to="/">
+                  Login
+                  <span class="sr-only">(current)</span>
+                </Link>
+              </li>
+              <li
+                class={`nav-item  ${
+                  props.location.pathname === "/register" ? "active" : ""
+                }`}
+              >
+                <Link class="nav-link" to="/register">
+                  Register
+                </Link>
+              </li>
+              <li
+                class={`nav-item  ${
+                  props.location.pathname === "/home" ? "active" : ""
+                }`}
+              >
+                <Link class="nav-link" to="/home">
+                  Home
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default withRouter(Navigation);
